eabd17387cafbe6cc65c5ea6935e539459b726fe
kazenesia <kazenesia@gmail.com>
2025-05-13 15:24:41 +0700

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,6 +1,5 @@
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
-const selectBtn = document.getElementById("select-btn");
 
 // Event listener untuk tombol sebelumnya
 prevBtn.addEventListener("click", () => {
@@ -12,14 +11,4 @@ prevBtn.addEventListener("click", () => {
 nextBtn.addEventListener("click", () => {
     currentIndex = (currentIndex + 1) % carModels.length;
     loadCar(scene, currentIndex);
-});
-
-// Event listener untuk memilih mobil
-selectBtn.addEventListener("click", () => {
-    selectedCar = currentCar;
-    document.getElementById("car-specs").innerHTML = carModels[currentIndex].specs;
-
-    // Batasi eksplorasi kamera hanya ke bagian atas dan samping
-    camera.lowerBetaLimit = Math.PI / 4; // Tidak bisa melihat bawah
-    camera.upperBetaLimit = Math.PI / 2; // Bisa melihat atas
 });
