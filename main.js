93b44fc8cc339bc4a1bb758c5df5656736ee9bee
kazenesia <kazenesia@gmail.com>
2025-05-18 15:28:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import * as THREE from '/three/build/three.module.js'; 
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js'
 
